215bb542299e7e71550237de7892887edbde6fe4
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-03-18 15:20:44 +0100

    fix makefiles, rename karen to harl

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -1,10 +1,10 @@
-#include "Karen.hpp"
+#include "Harl.hpp"
 
 int main() {
-	Karen karen;
+	Harl harl;
 
-	karen.complain("DEBUG");
-	karen.complain("INFO");
-	karen.complain("WARNING");
-	karen.complain("ERROR");
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
 }
